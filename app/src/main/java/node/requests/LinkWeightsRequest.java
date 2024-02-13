a8184ed886db7e2af744703ae06788771db62f8c
Brent Jones <brentjones135@gmail.com>
2024-02-13 13:55:36 -0700

    Progress toward dijkstras and link requests

--- app/src/main/java/node/requests/LinkWeightsRequest.java
+++ app/src/main/java/node/requests/LinkWeightsRequest.java
@@ -18,6 +18,7 @@ public class LinkWeightsRequest implements Event {
     private final int portNumber;
     private int numberOfLinks;
     private String linkInformation;
+    
     public LinkWeightsRequest(int requestType, String ipAddress, int portNumber, int numberOfLinks, String linkInformation) {
         this.requestType = requestType;
         this.ipAddress = ipAddress;
@@ -54,24 +55,34 @@ public class LinkWeightsRequest implements Event {
             String hostname2 = parsedLinks[i+2];
             int portNumber2 = Integer.valueOf(parsedLinks[i+3]);
             int weight = Integer.valueOf(parsedLinks[i+4]);
+            String comparisonString1 = hostname1 + portNumber1;
+            String comparisonString2 = hostname2 + portNumber2;
             RegisteredNodeData host1;
             RegisteredNodeData host2;
-            if (data.getConnection(hostname1) != null) {
-                host1 = new RegisteredNodeData(data.getConnection(hostname1), hostname1, portNumber1);
+            if (comparisonString1.compareTo(comparisonString2) > 0) {
+                if (data.getOverlay().containsIP(hostname1)) {
+                    host1 = data.getOverlay().getByIp(hostname1);
+                    if (host1 == null) {
+                        System.out.println("Issue converting Ip string to registered node data in link weights request 1");
+                    }
+                }
+                else {
+                    host1 = new RegisteredNodeData(hostname1, portNumber1);
+                    data.getOverlay().addRegisteredNode(host1);
+                }
+                if (data.getOverlay().containsIP(hostname2)) {
+                    host2 = data.getOverlay().getByIp(hostname2);
+                    if (host2 == null) {
+                        System.out.println("Issue converting Ip string to registered node data in link weights request 2");
+                    }
+                }
+                else {
+                    host2 = new RegisteredNodeData(hostname2, portNumber2);
+                    data.getOverlay().addRegisteredNode(host2);
+                }
+                host1.addNeighbor(hostname2, weight);
+                host2.addNeighbor(hostname1, weight);
             }
-            else {
-                host1 = new RegisteredNodeData(hostname1, portNumber1);
-            }
-            if (data.getConnection(hostname1) != null) {
-                host2 = new RegisteredNodeData(data.getConnection(hostname2), hostname2, portNumber2);
-            }
-            else {
-                host2 = new RegisteredNodeData(hostname2, portNumber2);
-            }
-            host1.addNeighbor(host2, weight);
-            host2.addNeighbor(host1, weight);
-            data.getOverlay().addRegisteredNode(host1);
-            data.getOverlay().addRegisteredNode(host2);
         }
     }
     @Override
--- app/src/main/java/registry/CommandExecutor.java
+++ app/src/main/java/registry/CommandExecutor.java
@@ -1,5 +1,34 @@
 package registry;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import node.NodeData;
+
 public class CommandExecutor extends Thread {
-    
+    NodeData nodeReference;
+    public CommandExecutor(NodeData data) {
+        nodeReference = data;
+    }
+    public void startCommandInput() {
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+        while(true) {
+            try {
+                String[] command = reader.readLine().split(" ");
+                switch(command[0]) {
+                    case "list-weights": break;
+                    case "list-messaging-nodes": break;
+                    case "setup-overlay number-of-connections": break;
+                    case "send-overlay-link-weights": break;
+                }
+
+            }
+            catch(IOException e) {
+
+            }
+        }
+    }
 }
